32c2d2a21e3ff89e1a08a1bb2a1d1c7771cdde4f
seamus tuohy <code@seamustuohy.com>
2022-01-17 11:42:03 -0500

    fix imports

--- biblio_web/biblio_web/urls.py
+++ biblio_web/biblio_web/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.urls import path, include
 from update_info import views
-import os.environ
+
 
 
 # Get a URL prefix if it exists
